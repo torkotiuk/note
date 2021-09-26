bf047cceffc141b1f475e3736e578ad82bf7fe0b
Andrii Torkotiuk <torkotiuk@gmail.com>
2021-09-26 21:29:18 +0200

    connect resdux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import './styles/bootstrap.min.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root'),
 );
