1d257e151ccfa0b8c4ed40f01c36b5e330dc7cac
Andrii Torkotiuk <torkotiuk@gmail.com>
2021-10-06 23:19:51 +0200

    deploy to heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./backend/bin/db-connection');
@@ -14,12 +15,23 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API notes backend is running......');
-});
-
 app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
+
+// ---------- deployment -----------
+__dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (_, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (_, res) => {
+    res.send('API notes backend is running......');
+  });
+}
+// ---------------------------------
 
 app.use(notFound);
 app.use(errorHandler);
